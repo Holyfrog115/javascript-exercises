9129c7e7a6ad15e8d27ab53e8b70113d1c5335fa
Holyfrog115 <149291415+Holyfrog115@users.noreply.github.com>
2026-05-20 15:15:18 +0700

    Add function for summing every int in range

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(intA, intB) {
+    let sum = 0;
+    let start = 0;
+    let end = 0;
 
+    if (Number.isInteger(intA) && Number.isInteger(intB) && intA > 0 && intB > 0) {
+        if (intA < intB) {
+            start = intA;
+            end = intB;
+        }
+        else {
+            start = intB;
+            end = intA;
+        }
+    }
+    else {
+        return "ERROR";
+    }
+    
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
