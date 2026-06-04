fbdee0ed9e96517b4fbe9afb055e5cf4cdbeb944
Holyfrog115 <149291415+Holyfrog115@users.noreply.github.com>
2026-06-04 10:00:38 +0700

    Add replaces for spaces and punctuation
    palindromes Function didn't work with strings that contain multiple
    words or punctuation.

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,5 @@
 const palindromes = function (string) {
-    stringLwr = string.toLowerCase();
+    stringLwr = string.toLowerCase().replaceAll(" ", "").replaceAll(",", "").replaceAll(".", "").replaceAll("!", "");
     for (let i = 0; i < stringLwr.length / 2; i++) {
         if (stringLwr.at(i) != stringLwr.at(-i - 1)) {
             return false;
