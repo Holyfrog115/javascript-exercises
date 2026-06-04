f6166c429a97445ca5c3f932a69719e026b39305
Holyfrog115 <149291415+Holyfrog115@users.noreply.github.com>
2026-06-04 10:22:46 +0700

    Add fibonacci function using queue

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(fNum) {
+    if (fNum < 3) {
+        return 1;
+    }
+    else {
+        let num = 0;
+        let previousNums = [1, 1];
+        for (let i = 3; i <= fNum; i++) {
+            num = previousNums[0] + previousNums[1];
+            previousNums.shift();
+            previousNums.push(num);
+        }
+        return num;
+    }
 };
 
 // Do not edit below this line
