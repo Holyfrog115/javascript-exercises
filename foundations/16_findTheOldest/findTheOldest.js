cd309a4a4bfb72f0626ec22e535c99469747c224
Holyfrog115 <149291415+Holyfrog115@users.noreply.github.com>
2026-06-05 10:41:15 +0700

    Add function for finding oldest person with sort

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    oldestToYoungest = people.sort((a, b) => {
+        let currentYear = new Date().getFullYear();
+        aAge = a.yearOfDeath ? a.yearOfDeath - a.yearOfBirth : currentYear - a.yearOfBirth;
+        bAge = b.yearOfDeath ? b.yearOfDeath - b.yearOfBirth : currentYear - b.yearOfBirth;
 
+        return bAge - aAge;
+    })
+
+    return oldestToYoungest[0];
 };
 
 // Do not edit below this line
